30f064d4328a333f9ab6068a62109bbf757d4c49
taaaaa111 <taos.tou@gmail.com>
2023-01-24 13:17:51 +0100

    initial commit with spring api

---
+++ src/main/java/packagename_employeemanager/NameEmployeemanagerApplication.java
@@ -0,0 +1,13 @@
+package packagename_employeemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NameEmployeemanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NameEmployeemanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/packagename_employeemanager/NameEmployeemanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package packagename_employeemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NameEmployeemanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
