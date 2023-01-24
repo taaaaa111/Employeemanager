aa6c1ec63bce599a888fc9818bcb92d7552ef491
taaaaa111 <taos.tou@gmail.com>
2023-01-24 18:26:13 +0100

    employee entity added with getter&setter, empty &  full constructor, toString

---
+++ src/main/java/packagename_employeemanager/model/Employee.java
@@ -0,0 +1,100 @@
+package packagename_employeemanager.model;
+
+import jakarta.persistence.*;
+
+
+import java.io.Serializable;
+
+@Entity
+public class Employee implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, updatable = false)
+    long id;
+    private String name;
+    private String firstName;
+    private String jobTitle;
+    private String phoneNumber;
+    private String imageUrl;
+    @Column(nullable = false, updatable = false)
+    private String employeeCode;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getEmployeeCode() {
+        return employeeCode;
+    }
+    public void setEmployeeCode(String employeeCode) {
+        this.employeeCode = employeeCode;
+    }
+    public Employee(long id, String name, String firstName, String jobTitle, String phoneNumber, String imageUrl, String employeeCode) {
+        this.id = id;
+        this.name = name;
+        this.firstName = firstName;
+        this.jobTitle = jobTitle;
+        this.phoneNumber = phoneNumber;
+        this.imageUrl = imageUrl;
+        this.employeeCode = employeeCode;
+    }
+    public Employee() {
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", jobTitle='" + jobTitle + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", imageUrl='" + imageUrl + '\'' +
+                ", employeeCode='" + employeeCode + '\'' +
+                '}';
+    }
+}
